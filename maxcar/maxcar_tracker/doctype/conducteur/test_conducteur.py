07754d36f79919c66234ca8a63028da4e55eca6f
fiikra <chaib.nassim@outlook.com>
2024-05-31 21:19:13 +0000

    master

---
+++ maxcar/maxcar_tracker/doctype/conducteur/test_conducteur.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, chaib.nassim@outlook.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestConducteur(FrappeTestCase):
+	pass
