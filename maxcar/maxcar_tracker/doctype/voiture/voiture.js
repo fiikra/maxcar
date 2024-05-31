b4ea786794f37422443602d16e78e6a14612b0d9
fiikra <chaib.nassim@outlook.com>
2024-05-31 21:35:26 +0000

    master

---
+++ maxcar/maxcar_tracker/doctype/voiture/voiture.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, chaib.nassim@outlook.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Voiture', {
+	// refresh: function(frm) {
+
+	// }
+});
